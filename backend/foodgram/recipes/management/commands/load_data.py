2947f366fadc01da38f72242c79bcc4b00b7f2e7
mign0n <mign0n.git@gmail.com>
2023-09-20 10:15:25 +0500

    feat(recipe): Add a django-admin command to load ingredients into the database.

---
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+from django.core.management.base import BaseCommand, CommandParser
+from recipes import models
+
+
+class Command(BaseCommand):
+    def handle(self, *args: tuple, **options: dict) -> None:
+        del args
+        with open(options['file'], encoding='utf-8') as file:
+            if not options.get('json'):
+                contents = csv.DictReader(
+                    file,
+                    fieldnames=('name', 'measurement_unit'),
+                )
+            else:
+                contents = json.load(file)
+            for row in contents:
+                models.Ingredient.objects.get_or_create(**row)
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            '-f',
+            '--file',
+            action='store',
+            help='Filepath to load, e.g. "data/ingredients.csv".',
+            required=True,
+            type=str,
+        )
+        parser.add_argument(
+            '--json',
+            action='store_true',
+            help='JSON file format.',
+        )
