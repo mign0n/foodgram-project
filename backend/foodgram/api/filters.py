436f63dc1e47c625819a342930260e1794c26dcb
mign0n <mign0n.git@gmail.com>
2023-10-12 11:15:44 +0500

    fix(api): Fix filtering by favorites and shopping cart.

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -15,33 +15,16 @@ class RecipeFilterSet(dj_filters.FilterSet):
         lookup_expr='icontains',
         queryset=Tag.objects.all(),
     )
-    is_favorited = dj_filters.BooleanFilter()
-    is_in_shopping_cart = dj_filters.BooleanFilter()
+    is_favorited = dj_filters.BooleanFilter(method='filter')
+    is_in_shopping_cart = dj_filters.BooleanFilter(method='filter')
 
-
-# class RecipeFilter(FilterSet):
-#     tags = ModelMultipleChoiceFilter(
-#         field_name='tags__slug',
-#         to_field_name='slug',
-#         queryset=Tags.objects.all()
-#     )
-#     author = ModelMultipleChoiceFilter(
-#         field_name='author__id',
-#         to_field_name='id',
-#         queryset=User.objects.all()
-#     )
-#     is_favorited = BooleanFilter(method='my_custom_filter')
-#
-#     class Meta:
-#         model = Recipes
-#         fields = (
-#             'tags','author', 'is_favorited',
-#         )
-
-# def my_custom_filter(self, queryset, name, value):
-#     name = 'favorite_recipes__user'
-#     if value == False:
-#         return queryset
-#     return queryset.filter(**{
-#         name: self.request.user,
-#     })
+    def filter(self, queryset, name, value):
+        query_lookup = {
+            'is_favorited': 'favorite__author',
+            'is_in_shopping_cart': 'cart__author',
+        }
+        if not value or not self.request.user.is_authenticated:
+            return queryset
+        return queryset.filter(
+            **{query_lookup.get(name, ''): self.request.user},
+        )
